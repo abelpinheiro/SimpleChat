b3bd8a916e6a0e99663c0ea3182dad8f92f319b2
abel <abelpinheiro95@gmail.com>
2020-02-25 00:15:38 -0300

    WebSocket Added. Message of new user in the chat is broadcasted to all members in chat

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // Importando as configurações do servidor
 let app = require('./config/server.js');
 
-// parametrização da porta de escuta
-
-app.listen(80, function() {
+// Parametrização da porta de escuta
+let server = app.listen(80, function() {
     console.log('servidor online');
-})
+});
+
+let io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+// Criando conexão com o websocket
+io.on('connection', function(socket){
+    console.log('Usuário conectou');
+
+    socket.on('disconnect', function(socket){
+        console.log('Usuário desconectou');
+    })
+});
--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,7 +1,7 @@
 module.exports.startChat = function(application, req, res){
     let username = req.body;
-    
-    req.assert('apelido', 'Nome ou apelido é obrigatório').isEmpty();
+    console.log(username);
+    //req.assert('apelido', 'Nome ou apelido é obrigatório').isEmpty();
     req.assert('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres').isLength({min: 3, max: 15});
 
     const errors = req.validationErrors();
@@ -9,5 +9,11 @@ module.exports.startChat = function(application, req, res){
         res.render('index', {validacao : errors});
         return; //res.status(422).json({errors: errors.array()})
     }
+
+    application.get('io').emit(
+        'msg', 
+        {apelido: username.apelido, mensagem: ' acabou de entrar no chat!'}
+        );
+
     res.render('chat');
 }
